4ac0fe65ec144c3e55d7fa045c5b8487d28cccf1
Eshan <eshansalwan@gmail.com>
2022-06-06 01:10:20 -0400

    Initial commit

---
+++ app/src/main/java/eshan/salwan/n01422232/EshanActivity.java
@@ -0,0 +1,15 @@
+//Eshan Salwan, n01422232, RNB
+package eshan.salwan.n01422232;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EshanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eshan_activity);
+    }
+}
---
+++ app/src/main/java/eshan/salwan/n01422232/SalwanActivity.java
@@ -0,0 +1,15 @@
+//Eshan Salwan, n01422232, RNB
+package eshan.salwan.n01422232;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SalwanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.salwan_activity);
+    }
+}
