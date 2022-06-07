540dd700d05360f19f28293448937682a3f6a4ae
Eshan <eshansalwan@gmail.com>
2022-06-07 19:11:50 -0400

    created 2 activities for apartment, detached homes, and semi-detached homes

--- app/src/main/java/eshan/salwan/n01422232/RentActivity.java
+++ app/src/main/java/eshan/salwan/n01422232/RentActivity.java
@@ -12,9 +12,6 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class RentActivity extends MenuActivity {
-    public static final String KEY1 = "img1";
-    public static final String KEY2 = "img2";
-    public static final String KEY3 = "img3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +30,12 @@ public class RentActivity extends MenuActivity {
 
                 if (rb1.isChecked()) {
                     Intent aprt = new Intent(RentActivity.this, VisitActivity.class);
-                    aprt.putExtra("text", "Apartment Options");
-                    aprt.putExtra(KEY1, R.drawable.apartment1);
-                    aprt.putExtra(KEY2, R.drawable.apartment2);
-                    aprt.putExtra(KEY3, R.drawable.apartment2);
-                    aprt.putExtra("CB1", "");
-                    aprt.putExtra("CB2", "");
-                    aprt.putExtra("CB3", "");
                     startActivity(aprt);
                 } else if (rb2.isChecked()) {
                     Intent detach = new Intent(RentActivity.this, VisitActivity.class);
-                    detach.putExtra("text", "Detached Home Options");
-                    detach.putExtra(KEY1, R.drawable.detached1);
-                    detach.putExtra(KEY2, R.drawable.detached2);
-                    detach.putExtra(KEY3, R.drawable.detached3);
-                    detach.putExtra("CB1", "");
-                    detach.putExtra("CB2", "");
-                    detach.putExtra("CB3", "");
                     startActivity(detach);
                 } else if (rb3.isChecked()) {
                     Intent semi = new Intent(RentActivity.this, VisitActivity.class);
-                    semi.putExtra("text", "Semi-Detached Home Options");
-                    semi.putExtra(KEY1, R.drawable.semi1);
-                    semi.putExtra(KEY2, R.drawable.semi2);
-                    semi.putExtra(KEY3, R.drawable.semi3);
-                    semi.putExtra("CB1", "");
-                    semi.putExtra("CB2", "");
-                    semi.putExtra("CB3", "");
                     startActivity(semi);
                 }
 
--- app/src/main/java/eshan/salwan/n01422232/VisitActivity.java
+++ app/src/main/java/eshan/salwan/n01422232/VisitActivity.java
@@ -18,67 +18,6 @@ public class VisitActivity extends MenuActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visit_activity);
-       /* Intent aprt = getIntent();
-        Intent detach = getIntent();
-        Intent semi = getIntent();
-        String text1 = aprt.getStringExtra("text");
-        String text2 = detach.getStringExtra("text");
-        String text3 = semi.getStringExtra("text");
-        ((TextView)findViewById(R.id.textView)).setText(text1); */
 
-        RadioButton rb1 = (RadioButton) findViewById(R.id.radioButton);
-        RadioButton rb2 = (RadioButton) findViewById(R.id.radioButton2);
-        RadioButton rb3 = (RadioButton) findViewById(R.id.radioButton3);
-
-        if (rb1.isChecked()) {
-            Intent aprt = getIntent();
-            String text = aprt.getStringExtra("text");
-            ((TextView)findViewById(R.id.textView)).setText(text);
-/*
-            Intent intent = getIntent();
-            ImageView img1 = findViewById(R.id.imageView2);
-            int aimg1 = intent.getIntExtra(RentActivity.KEY1,1);
-            img1.setImageResource(aimg1);
-
-            ImageView img2 = findViewById(R.id.imageView3);
-            int aimg2 = intent.getIntExtra(RentActivity.KEY2,1);
-            img2.setImageResource(aimg2);
-
-            ImageView img3 = findViewById(R.id.imageView3);
-            int aimg3 = intent.getIntExtra(RentActivity.KEY3,1);
-            img3.setImageResource(aimg3);
-
-*/
-        } else if (rb2.isChecked()) {
-            Intent detach = getIntent();
-
-            String text = detach.getStringExtra("text");
-            ((TextView)findViewById(R.id.textView)).setText(text);
-            /*
-            detach.putExtra("img1", "");
-            detach.putExtra("img2", "");
-            detach.putExtra("img3", "");
-            detach.putExtra("CB1", "");
-            detach.putExtra("CB2", "");
-            detach.putExtra("CB3", "");
-            startActivity(detach);
-
-             */
-        } else if (rb3.isChecked()) {
-            Intent semi = getIntent();
-
-            String text = semi.getStringExtra("text");
-            ((TextView)findViewById(R.id.textView)).setText(text);
-            /*
-            semi.putExtra("img1", "");
-            semi.putExtra("img2", "");
-            semi.putExtra("img3", "");
-            semi.putExtra("CB1", "");
-            semi.putExtra("CB2", "");
-            semi.putExtra("CB3", "");
-            startActivity(semi);
-
-             */
-        }
     }
 }
